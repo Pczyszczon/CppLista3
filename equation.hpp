7af5536f13104f84a9ac63976ca4a5c27493b499
Pczyszczon <290425@uwr.edu.pl>
2018-11-06 13:00:16 +0100

    public interface

---
+++ equation.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+class equation{
+    public:
+        int a;
+        int b;
+        int c;
+    equation(int val_a, int val_b, int val_c){
+        a = val_a;
+        b = val_b;
+        c = val_c;
+    }
+    equation(){
+        a = 0;
+        b = 0;
+        c = 0;
+    }
+};
